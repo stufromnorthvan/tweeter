e5897b63f1df4e7edb23c4d2feaa9d5cb0fff969
stufromnorthvan <stumitchell@irsss.ca>
2022-10-13 09:43:37 -0700

    added functions into client.js that fetch existing tweets from the server.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-const renderTweets = function(tweets) {
-  for (let tweet of data)
-    $('.tweets-container').append(createTweetElement(tweet));
-};
-
 const createTweetElement = function(data) {
   let $tweet = `<article class="tweet">
     <header>
@@ -58,7 +26,20 @@ const createTweetElement = function(data) {
   return $tweet;
 };
 
-renderTweets(data);
+const renderTweets = function(data) {
+  for (let tweet of data)
+    $('.tweets-container').append(createTweetElement(tweet));
+};
+
+const loadTweets = () => {
+  $.ajax('/tweets', { method: 'GET' })
+    .then(function(getTweets) {
+      renderTweets(getTweets);
+      console.log(`Success, tweets loaded!`);
+    });
+};
+
+loadTweets();
 
 $(document).ready(function() {
   console.log("client.js is active");
@@ -69,7 +50,7 @@ $(document).ready(function() {
     event.preventDefault();
     console.log("event prevented, form submitted");
     const formData = ($tweetform.serialize());
-    console.log("tweet being sent to server from current user: ", formData);
+    console.log("tweet currently being sent to server from current user: ", formData);
     //sends form data to server
     $.ajax({
       method: 'POST',
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -24,6 +24,7 @@ module.exports = function(DataHelpers) {
     }
     
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    
     const tweet = {
       user: user,
       content: {
