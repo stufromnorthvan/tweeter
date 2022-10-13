0dcc6272f4bb89a2fb2a9b110c2b5fa3f6b2c81e
stufromnorthvan <stumitchell@irsss.ca>
2022-10-12 21:15:57 -0700

    rendered tweets from a pre-existing database with my css stylings

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,66 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  for (let tweet of data)
+  $('.tweets-container').append(createTweetElement(tweet));
+}
+
+const createTweetElement = function(data) {
+  let $tweet = `<article class="tweet">
+    <header>
+    <span class="pic-and-name">
+      <div class="avatar"><img src=${data.user.avatars}></div>
+      <div class="username">${data.user.name}</div>
+    </span>
+      <div class="handle">${data.user.handle}</div>
+    </header>
+    <div class = "message">${data.content.text}</div>
+    <footer>
+      <div>${timeago.format(data.created_at)}</div>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
+  return $tweet;
+};
+
+renderTweets(data)
+
+// const $tweet = createTweetElement(tweetData);
+// console.log($tweet)
+// $('.tweets-container').append($tweet); 
+
+// $(document).ready(function() {
+
+
+// });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,12 @@
 // Script for character counter
 
 $(document).ready(function() {
-  console.log("is ya'll ready?!");
-  const $tweetform = $('.tweet-form');
+  console.log("- character counter is active.");
   const $counter = $('.counter');
   const $textarea = $('#tweet-text');
   $textarea.on('input', () => {
     let textLength = $textarea.val().length;
     let diff = 140 - textLength;
-    console.log(diff);
     $counter.text(diff);
     if (diff < 0) {
       $counter.addClass('max-reached');
