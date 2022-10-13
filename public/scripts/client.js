42e00ae1e649a182445774d12d47839dd0f81faf
stufromnorthvan <stumitchell@irsss.ca>
2022-10-12 22:49:42 -0700

    added event listener on form that sends form data to server with jquery and ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -22,18 +22,19 @@ const data = [
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
   }
-]
+];
 
 const renderTweets = function(tweets) {
   for (let tweet of data)
-  $('.tweets-container').append(createTweetElement(tweet));
-}
+    $('.tweets-container').append(createTweetElement(tweet));
+};
 
 const createTweetElement = function(data) {
   let $tweet = `<article class="tweet">
@@ -57,13 +58,29 @@ const createTweetElement = function(data) {
   return $tweet;
 };
 
-renderTweets(data)
+renderTweets(data);
 
-// const $tweet = createTweetElement(tweetData);
-// console.log($tweet)
-// $('.tweets-container').append($tweet); 
-
-// $(document).ready(function() {
-
-
-// });
+$(document).ready(function() {
+  console.log("client.js is active");
+  //grab form
+  const $tweetform = $('.tweet-form');
+  //listen for event
+  $tweetform.on("submit", (event) => {
+    event.preventDefault();
+    console.log("event prevented, form submitted");
+    const formData = ($tweetform.serialize());
+    console.log("tweet being sent to server from current user: ", formData);
+    //sends form data to server
+    $.ajax({
+      method: 'POST',
+      url: './tweets',
+      data: formData
+    })
+      .then((response) => {
+        console.log("response: ", response);
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  });
+});
